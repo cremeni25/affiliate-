4802efdd62cf0b434bc04304ea1e6acad748155a
cremeni25 <andersonnavarro.si@gmail.com>
2025-12-24 03:43:54 -0300

    Create clickbank.py

---
+++ affiliate/clickbank.py
@@ -0,0 +1,23 @@
+# affiliate/clickbank.py
+class ClickBankAdapter(AffiliateProvider):
+platform = "clickbank"
+
+
+def search_products(self, filters: Dict[str, Any]) -> List[AffiliateProduct]:
+raise NotImplementedError
+
+
+def get_product(self, external_id: str) -> AffiliateProduct:
+raise NotImplementedError
+
+
+def generate_affiliate_link(self, external_id: str) -> str:
+raise NotImplementedError
+
+
+def validate_affiliation(self, external_id: str) -> bool:
+raise NotImplementedError
+
+
+def normalize_product(self, raw_data: Dict[str, Any]) -> AffiliateProduct:
+raise NotImplementedError
