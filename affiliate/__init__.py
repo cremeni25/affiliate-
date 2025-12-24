b4663ee96c6106de7bbaa94689bd9e65ab1e4d5d
cremeni25 <andersonnavarro.si@gmail.com>
2025-12-24 03:55:09 -0300

    Create __init__.py

---
+++ affiliate/__init__.py
@@ -0,0 +1,5 @@
+from .base import AffiliateProvider, AffiliateProduct
+from .hotmart import HotmartAdapter
+from .eduzz import EduzzAdapter
+from .monetizze import MonetizzeAdapter
+from .clickbank import ClickBankAdapter
